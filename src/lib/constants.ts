1eec9a34881b8c2b55b791c05bdc82522febe208
iDsani <112170074+iDsani@users.noreply.github.com>
2022-12-18 12:45:59 +0800

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -459,7 +459,7 @@ export const pets: {
     //Goomba stacks
     {
         name: "Goomba Stack Pro Max Ultra",
-        perks: (level) => ({hitAdd: level*0.075, hitMul: level*0.075*level + Math.random()}),
+        perks: (level) => ({hitAdd: level*0.075, hitMul: 0.075*level + Math.random()}),
         description: "Goomba Stack Pro Max goes too far",
         salvage: 600,
         rarity: "mythical",
@@ -467,7 +467,7 @@ export const pets: {
     },
     {
         name: "Goombascraper",
-        perks: (level) => ({hitAdd: level*0.1, hitMul: level*0.1*level + Math.random()}),
+        perks: (level) => ({hitAdd: level*0.1, hitMul: 0.1*level + Math.random()}),
         description: "Goomba Stack Pro Max goes too far its a skyscraper",
         salvage: 1200,
         rarity: "ChRoMaTiC",
@@ -475,7 +475,7 @@ export const pets: {
     },
     {
         name: "Goombascraper Ultra",
-        perks: (level) => ({hitAdd: level*0.125, hitMul: level*0.125*level + Math.random()}),
+        perks: (level) => ({hitAdd: level*0.125, hitMul: 0.125*level + Math.random()}),
         description: "Goombascraper, but more",
         salvage: 2400,
         rarity: "ChRoMaTiC",
